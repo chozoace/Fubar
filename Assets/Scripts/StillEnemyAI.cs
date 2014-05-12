937a5f6bd60394c1306ede9cab4e3c1d2feb0cf5
FrozenxPoptart <frozenxpoptart@gmail.com>
2014-05-12 15:33:14 -0400

    Semi-Added Enemy Death Animation

--- Assets/Scripts/StillEnemyAI.cs
+++ Assets/Scripts/StillEnemyAI.cs
@@ -12,15 +12,27 @@ public class StillEnemyAI : MonoBehaviour
 	GameObject player;
 	Animator anim;
 
-	int facing = 1;//1 = left, 0 = right
+	int facing = 1;//1 = left, 0 = right //This guy
 	int yDirection;
 	public float detectionRange = 5f;
 	public Gun myGun;
+
+	private int _deathRightStateID;
+	private int _deathLeftStateID;
+
+	private bool _isDead;
+
+	void Awake() {
+		_deathRightStateID = Animator.StringToHash("Base Layer.EnemyDeathRight");
+		_deathLeftStateID = Animator.StringToHash("Base Layer.EnemyDeathLeft");
+	}
 	// Use this for initialization
 	void Start () 
 	{
+		facing = 1;
 		myGun.identifyOwner(this.gameObject);
-		anim = GetComponent<Animator>();	
+		anim = GetComponent<Animator>();
+		_isDead = false;
 	}
 	
 	public int getFacing()
@@ -33,57 +45,59 @@ public class StillEnemyAI : MonoBehaviour
 	{
 		if(GameController.Instance().levelState == GameController.LevelState.GamePlay)
 		{
-			player = GameObject.FindGameObjectWithTag("Player");
-			//for facing
-			if(this.transform.position.x - player.transform.position.x < 0)
-			{
-				facing = 0;
-				anim.SetInteger("facing", facing);
-			}
-			else
-			{
-				facing = 1;
-				anim.SetInteger("facing", facing);
-			}
-			//for distance check
-			if(this.transform.position.x - player.transform.position.x < detectionRange && facing == 1)
-			{
-				playerInSight = true;			
-			}
-			else if(player.transform.position.x - this.transform.position.x < detectionRange && facing == 0)
-			{
-				playerInSight = true;
-			}
-			else 
-				playerInSight = false;
-			//for y angled shooting
-			if(this.transform.position.y - player.transform.position.y < -.3)
-			{
-				yDirection = 1;
-				anim.SetBool("AimUp", true);
-				anim.SetBool("AimDown", false);
-			}
-			else if(this.transform.position.y - player.transform.position.y > .3)
-			{
-				yDirection = -1;
-				anim.SetBool("AimUp", false);
-				anim.SetBool("AimDown", true);
-			}
-			else 
-			{
-				yDirection = 0;
-				anim.SetBool("AimUp", false);
-				anim.SetBool("AimDown", false);
-			}
-			//detect enemy
-			//begin shooting at enemy, straight shots
-			if(playerInSight)
-			{
-				if(canShoot)
+			if(_isDead == false) {
+				player = GameObject.FindGameObjectWithTag("Player");
+				//for facing
+				if(this.transform.position.x - player.transform.position.x < 0)
 				{
-					canShoot = myGun.shoot (facing, yDirection);
-					canShoot = false;
-					Invoke("reload", fireRate);;
+					facing = 0;
+					anim.SetInteger("facing", facing);
+				}
+				else
+				{
+					facing = 1;
+					anim.SetInteger("facing", facing);
+				}
+				//for distance check
+				if(this.transform.position.x - player.transform.position.x < detectionRange && facing == 1)
+				{
+					playerInSight = true;			
+				}
+				else if(player.transform.position.x - this.transform.position.x < detectionRange && facing == 0)
+				{
+					playerInSight = true;
+				}
+				else 
+					playerInSight = false;
+				//for y angled shooting
+				if(this.transform.position.y - player.transform.position.y < -.3)
+				{
+					yDirection = 1;
+					anim.SetBool("AimUp", true);
+					anim.SetBool("AimDown", false);
+				}
+				else if(this.transform.position.y - player.transform.position.y > .3)
+				{
+					yDirection = -1;
+					anim.SetBool("AimUp", false);
+					anim.SetBool("AimDown", true);
+				}
+				else 
+				{
+					yDirection = 0;
+					anim.SetBool("AimUp", false);
+					anim.SetBool("AimDown", false);
+				}
+				//detect enemy
+				//begin shooting at enemy, straight shots
+				if(playerInSight)
+				{
+					if(canShoot)
+					{
+						canShoot = myGun.shoot (facing, yDirection);
+						canShoot = false;
+						Invoke("reload", fireRate);;
+					}
 				}
 			}
 		}
@@ -101,8 +115,33 @@ public class StillEnemyAI : MonoBehaviour
 		
 		if(health <= 0)
 		{
-			Destroy (gameObject);
+			//Destroy (gameObject);
+			EnemyDeath();
 			player.GetComponent<ScoreUI>().adjustScore(100);
+		}
+	}
+
+	public void EnemyDeath() {
+
+		_isDead = true;
+		anim.SetBool("isDead", _isDead);
+
+		if(facing == 1) { //Right
+			anim.Play (_deathRightStateID, 0, 0);
+		}
+		else if(facing == 0) {
+			anim.Play (_deathLeftStateID, 0, 0);
+		}
+
+		Debug.Log(anim.IsInTransition(0));
+		if(anim.IsInTransition(0) && anim.GetCurrentAnimatorStateInfo(0).nameHash == 154171580) {
+			Debug.Log("D");
+			GameObject.Destroy(this.gameObject);
+		}
+
+		if(anim.IsInTransition(0) && anim.GetCurrentAnimatorStateInfo(0).nameHash == 889664582) {
+			Debug.Log("D");
+			GameObject.Destroy(this.gameObject);
 		}
 	}
 }
