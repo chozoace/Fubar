df08342d5ae3a68fb533678d4184d955be1ed452
chozoace <chozoace@gmail.com>
2014-05-21 11:15:41 -0400

    Finished credits and tweaked enemy death

--- Assets/Scripts/CreditsScript.cs
+++ Assets/Scripts/CreditsScript.cs
@@ -4,18 +4,52 @@ using System.Collections;
 public class CreditsScript : MonoBehaviour 
 {
 	[SerializeField] KeyCode closeGame;
+    bool readyToClose = false;
+    bool startUpdate = false;
+
+    float fadeCounter = .02f;
 	
 	void Start ()
 	{
-	
+        Color transparentColor = this.GetComponent<SpriteRenderer>().color;
+        transparentColor.a = 0;
+        this.GetComponent<SpriteRenderer>().color = transparentColor;
+        Invoke("beginUpdate", 2f);
+        //readyToClose = true;
+        //startUpdate = true;
 	}
+
+    void beginUpdate()
+    {
+        startUpdate = true;
+    }
+
+    void fadeIn()
+    {
+        Color currentAlpha = this.GetComponent<SpriteRenderer>().color;
+        currentAlpha.a = Mathf.Clamp(this.GetComponent<SpriteRenderer>().color.a + fadeCounter, 0, 1);
+        this.GetComponent<SpriteRenderer>().color = currentAlpha;
+        //Debug.Log (currentAlpha);
+
+        if (currentAlpha.a == 1)
+        {
+            readyToClose = true;
+        }
+    }
 	
 	void Update () 
 	{
-		if(Input.GetKeyDown (closeGame))
-		{
-			Debug.Log ("pressed");
-			Application.Quit();
-		}
+        if (startUpdate)
+        {
+            if (Input.GetKeyDown(closeGame) && readyToClose)
+            {
+                Debug.Log("pressed");
+                Application.Quit();
+            }
+            else if (!readyToClose)
+            {
+                fadeIn();
+            }
+        }
 	}
 }
